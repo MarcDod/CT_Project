3be3291d92382c7cb38d59c737e0163962dc1a8f
Caeasarus <julian.reith@arcor.de>
2018-12-13 10:02:10 +0100

    Login implementation

---
+++ CT_Project/src/GuiElements/TextField.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GuiElements;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Julian
+ */
+public class TextField extends JPasswordField{
+    
+    private String hintText;
+    
+    public TextField(Color bg, String hintText,boolean password){
+        this.setBorder(null);
+        this.setBackground(bg);
+        this.hintText = hintText;
+        this.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        if(!password){
+            this.setEchoChar((char) 0);
+        }
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawLine(0, this.getHeight() - 1, this.getWidth(), this.getHeight() - 1);
+        if(this.getText().equals("")){
+            FontMetrics fm = g.getFontMetrics();
+            g.setColor(Color.decode("0x838B8B"));
+            g.drawString(hintText, (this.getWidth() - 6)/2 - fm.stringWidth(hintText) / 2,this.getHeight() / 2 + fm.getHeight() / 4);
+        }
+    }
+}
--- CT_Project/src/GuiElements/activities/LoginScreen.java
+++ CT_Project/src/GuiElements/activities/LoginScreen.java
@@ -6,12 +6,16 @@
 package GuiElements.activities;
 
 import GuiElements.Button;
+import GuiElements.TextField;
 import ct_project.Gui;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import javax.swing.JTextField;
 
 /**
@@ -22,17 +26,20 @@ public class LoginScreen extends Activity{
     
     private Button loginButton;
     
-    private JTextField userName;
-    private JTextField password;
+    private TextField userName;
+    private TextField password;
     
     private LogInManager logInManager;
     
+    private Color background;
+    
     public LoginScreen(ActionListener loginButton, LogInManager logInManager) {
         super(ActivityID.LOGIN_SCREEN, Gui.SCREEN_HEIGHT - 29);
-        
+        this.background = Color.WHITE;
         this.logInManager = new LogInManager();
         
-        this.loginButton = new Button(Gui.SCREEN_WIDTH, 50);
+        this.loginButton = new Button(Gui.SCREEN_WIDTH, 50, getLoginImage(Gui.SCREEN_WIDTH,
+                50));
         this.loginButton.setLocation(0, this.getHeight() - this.loginButton.getHeight());
         this.loginButton.addActionListener(loginButton);
         this.loginButton.addActionListener(new ActionListener() {
@@ -42,22 +49,38 @@ public class LoginScreen extends Activity{
             }
         });
         
-        this.userName = initTextField(300);
-        this.password = initTextField(350);
+        this.userName = initTextField(300, "Username",false);
+        this.password = initTextField(350, "Password",true);
         
         this.add(this.loginButton);
         this.add(this.userName);
         this.add(this.password);
     }
     
-    private JTextField initTextField(int y){
-        JTextField retTextField = new JTextField();
-        retTextField.setSize((int) (Gui.SCREEN_WIDTH * 0.9), 40);
+    private TextField initTextField(int y, String hintText,boolean password){
+        TextField retTextField = new TextField(background, hintText,password);
+        retTextField.setSize((int) (Gui.SCREEN_WIDTH * 0.8), 20);
         retTextField.setLocation((Gui.SCREEN_WIDTH - 6) / 2 - retTextField.getWidth() / 2, y);
         
         retTextField.setHorizontalAlignment(JTextField.CENTER);
         
         return retTextField;
+    }
+    
+    private BufferedImage getLoginImage(int width, int height){
+        BufferedImage image = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
+        
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.decode("0x0000F0"));
+        g2d.fillRect(0, 0, width, height);
+        
+        g2d.setFont(new Font("SansSerif", Font.BOLD, 15));
+        FontMetrics fm = g2d.getFontMetrics();
+        g2d.setColor(Color.WHITE);
+        g2d.drawString("LOGIN", (width - 6)/2 - fm.stringWidth("LOGIN") / 2,height / 2 + fm.getHeight() / 4);
+        
+        g2d.dispose();
+        return image;
     }
     
      @Override
@@ -70,7 +93,7 @@ public class LoginScreen extends Activity{
         g2d.fillRect(0, 0, this.getParent().getWidth(),getHeight());
         
         // Background
-        g2d.setColor(new Color(240, 240, 240));
+        g2d.setColor(background);
         g2d.fillRect(0, 0, this.getParent().getWidth() - 6,getHeight());
         
         g2d.setColor(oldColor);
