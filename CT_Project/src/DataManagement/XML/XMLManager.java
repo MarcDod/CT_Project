5e21309b1d3a7f05e4c66abba04358c10d7f3f88
Caeasarus <julian.reith@arcor.de>
2018-12-21 16:33:38 +0100

    XML Orderlist now saving as list

--- CT_Project/src/DataManagement/XML/XMLManager.java
+++ CT_Project/src/DataManagement/XML/XMLManager.java
@@ -9,7 +9,9 @@ import DataManagement.Datatemplates.Orderlist;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
@@ -24,15 +26,24 @@ public class XMLManager{
     public XMLManager(){
     }
 
-    public Orderlist loadXMLOrderList(File file) throws JDOMException,
+    public ArrayList<Orderlist> loadXMLOrderLists(File file) throws JDOMException,
             IOException{
         Document xmlDoc = new SAXBuilder().build(file);
-        return new Orderlist(xmlDoc.getRootElement());
+        ArrayList<Orderlist> list = new ArrayList<>();
+        Element rootElement = xmlDoc.getRootElement();
+        for(Element element : rootElement.getChildren()){
+            list.add(new Orderlist(element));
+        }
+        return list;
     }
 
-    public void saveXMLOrderList(Orderlist o, File file) throws IOException{
+    public void saveXMLOrderLists(ArrayList<Orderlist> list, File file) throws IOException{
         FileWriter fw = new FileWriter(file);
+        Element root  = new Element("Orderlists");
+        list.forEach((orderlist) -> {
+            root.addContent(orderlist.pack());
+        });
         new XMLOutputter(Format.getPrettyFormat()).
-                output(new Document(o.pack()), fw);
+                output(new Document(root), fw);
     }
 }
