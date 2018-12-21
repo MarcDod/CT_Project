66da11924db865388407cc94edf3d4f9db00bb40
Caeasarus <julian.reith@arcor.de>
2018-12-21 13:19:19 +0100

    Order Database implementation Order *changed to fit new needs 	*date is now a java.sql.Date 	*deadline is now a String 	*exchanged accountID for user 	+added watched attribute Connector 	+added Order request by id

--- CT_Project/src/DataManagement/Datatemplates/Order.java
+++ CT_Project/src/DataManagement/Datatemplates/Order.java
@@ -1,5 +1,7 @@
 package DataManagement.Datatemplates;
 
+import java.sql.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,44 +14,46 @@ package DataManagement.Datatemplates;
  */
 public class Order {
     private int orderID;
-    private int date;
-    private int deadline;
+    private Date date;
+    private String deadline;
     private int number;
     private boolean closed;
     private String itemName;
-    private int accountID;
+    private String user;
+    private boolean watched;
 
-    public Order(int orderID, int date, int deadline, int number, boolean closed, String itemName, int accountID) {
+    public Order(int orderID, Date date, String deadline, int number, boolean closed, String itemName, String user , boolean watched){
         this.orderID = orderID;
         this.date = date;
         this.deadline = deadline;
         this.number = number;
         this.closed = closed;
         this.itemName = itemName;
-        this.accountID = accountID;
+        this.user = user;
+        this.watched = watched;
     }
 
     public int getOrderID() {
         return orderID;
     }
 
-    public void setOrderID(int orderID) {
-        this.orderID = orderID;
-    }
+//    public void setOrderID(int orderID) {
+//        this.orderID = orderID;
+//    }
 
-    public int getDate() {
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(int date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
-    public int getDeadline() {
+    public String getDeadline() {
         return deadline;
     }
 
-    public void setDeadline(int deadline) {
+    public void setDeadline(String deadline) {
         this.deadline = deadline;
     }
 
@@ -73,18 +77,29 @@ public class Order {
         return itemName;
     }
 
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
+//    public void setItemName(String itemName) {
+//        this.itemName = itemName;
+//    }
+
+//    public int getAccountID() {
+//        return accountID;
+//    }
+
+//    public void setAccountID(int accountID) {
+//        this.accountID = accountID;
+//    }
+
+    public String getUser(){
+        return user;
     }
 
-    public int getAccountID() {
-        return accountID;
+    public boolean isWatched(){
+        return watched;
     }
 
-    public void setAccountID(int accountID) {
-        this.accountID = accountID;
+    public void setWatched(boolean watched){
+        this.watched = watched;
     }
-    
     
     
 }
